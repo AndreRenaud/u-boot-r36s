30edc069c778ec340f8f09e08a1d9e778a6c7e9f
Andre Renaud <andre@ignavus.net>
2024-06-20 16:16:54 +1200

    Initial commit of basic R36S support

---
+++ include/configs/rk3326-r36s.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * (C) Copyright 2017 Rockchip Electronics Co., Ltd
+ */
+
+#ifndef __R36S_H
+#define __R36S_H
+
+#include <configs/px30_common.h>
+
+#define ROCKCHIP_DEVICE_SETTINGS ""
+
+#endif
